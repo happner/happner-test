9478b1b1b1123ca81105fc42fa5d2f7aee5fd984
nomilous <nomilous@gmail.com>
2015-11-05 18:36:41 +0200

    secure

--- conf/master.js
+++ conf/master.js
@@ -3,15 +3,21 @@ try {
   require('dotenv-safe').load();
 } catch (e) {
   console.error(e.message);
-  console.error('\nsuggestion: copy and ammend .env.sample to .env');
+  console.error('\nsuggestion: copy and amend .env.sample to .env');
   process.exit(1);
 }
 
 module.exports = {
 
   name: 'master',
-  host: process.env.MASTER_PUBLIC_ADDRESS,
-  port: process.env.MASTER_PUBLIC_PORT,
+
+  datalayer: {
+    secure: true,
+    adminPassword: process.env.ADMIN_PASSWORD,
+    // persist: true,
+    host: process.env.MASTER_PUBLIC_ADDRESS,
+    port: process.env.MASTER_PUBLIC_PORT,
+  },
 
   components: {
     'www': {
